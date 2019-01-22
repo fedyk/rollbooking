2b1b38441e03d5867b27eb85daa593f198576446
Andrii Fedyk <a.fedyk@livechatinc.com>
2019-01-22 17:44:06 +0100

    remove tests for calendar

--- packages/calendar/src/components/Calendar/Calendar.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { create } from 'react-test-renderer';
-
-import { Calendar } from './Calendar';
-
-it('renders correctly', () => {
-  const tree = create(<Calendar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
