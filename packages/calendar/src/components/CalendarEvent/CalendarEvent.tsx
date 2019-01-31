50ad2b9ae605e7113b27e45cb4372794f68ef5ab
Andrew <fedyk72@gmail.com>
2019-01-31 07:58:56 +0100

    Use CalendarContext in CalendarEvent

--- packages/calendar/src/components/CalendarEvent/CalendarEvent.tsx
+++ packages/calendar/src/components/CalendarEvent/CalendarEvent.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 // import * as Bootstrap from "bootstrap";
 import { DateLocalizer } from "react-big-calendar";
+import { Event } from "../../types";
+import { CalendarContext } from "../CalendarContext";
 
 declare const bootstrap: any;
 
 interface Props {
-  event: any;
+  event: Event;
   title: string;
   isAllDay: boolean;
   localizer: DateLocalizer;
@@ -25,14 +27,22 @@ export class CalendarEvent extends React.PureComponent<Props> {
   }
 
   componentDidMount() {
-    this.popover = new bootstrap.Popover(this.container.current.parentElement.parentElement, {
-      animation: false,
-      container: false,
-      content: this.popoverContent.current,
-      title: "title",
-      trigger: "manual",
-      template: `<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>`
-    })
+    this.popover = new bootstrap.Popover(
+      this.container.current.parentElement.parentElement,
+      {
+        animation: false,
+        container: false,
+        html: true,
+        content: this.popoverContent.current,
+        title: "title",
+        trigger: "manual",
+        template: `<div class="popover" role="tooltip">
+        <div class="arrow"></div>
+        <h3 class="popover-header"></h3>
+        <div class="popover-body"></div>
+      </div>`
+      }
+    );
   }
 
   componentWillUnmount() {
@@ -41,18 +51,48 @@ export class CalendarEvent extends React.PureComponent<Props> {
 
   componentDidUpdate(prevProps: Props) {
     if (this.props.event.showPopover && !prevProps.event.showPopover) {
-      this.popover.show()
+      this.popover.show();
     }
-    
+
     if (!this.props.event.showPopover && prevProps.event.showPopover) {
-      this.popover.hide()
+      this.popover.hide();
     }
   }
 
+  deleteEvent = () => {
+    this.popover.hide();
+    this.context.deleteEvent(this.props.event.id);
+  };
+
+  openEventModal = () => {
+    this.popover.hide();
+    this.context.openEventModal(this.props.event.id);
+  }
+
   render() {
-    return <div ref={this.container}>
-      {this.props.title}
-      <div ref={this.popoverContent} hidden>sdfsdfskdf</div>
-    </div>;
+    return (
+      <div ref={this.container}>
+        {this.props.title}
+
+        <div hidden>
+            <div ref={this.popoverContent}>
+              <div className="mb-2">
+                <div className="font-weight-bold">Service</div>
+                <div>{this.props.event.serviceName || "None"}</div>
+              </div>
+              <div className="mb-2">
+                <div className="font-weight-bold">Client</div>
+                <div>{this.props.event.userName || "None"}</div>
+              </div>
+              <div className="d-flex justify-content-between">
+                <button className="btn btn-sm btn-outline-danger" onClick={this.deleteEvent}>Delete</button>
+                <button className="btn btn-sm btn-primary" onClick={this.openEventModal}>Edit</button>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
   }
 }
+
+CalendarEvent.contextType = CalendarContext;
