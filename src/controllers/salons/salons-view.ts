149cc15596e8fbe9cf415baa998109718a1b4e64
Andrew <fedyk72@gmail.com>
2019-03-30 22:21:42 +0100

    Update /salons page

--- src/controllers/salons/salons-view.ts
+++ src/controllers/salons/salons-view.ts
@@ -14,7 +14,6 @@ export const salonsView = (props: Props) => `
       <tr>
         <th scope="col">Name</th>
         <th scope="col">Timezone</th>
-        <th scope="col">Amount of masters</th>
         <th scope="col"></th>
       </tr>
     </thead>
@@ -22,14 +21,13 @@ export const salonsView = (props: Props) => `
       ${stringMapJoin(props.salons, (salon) => `<tr>
         <td>
           ${salon.name}
-          <br/>
-          <small class="text-secondary">id: ${salon._id.toHexString()}</small>
+          <small class="text-secondary d-block">id: ${salon._id.toHexString()}</small>
+          <small class="text-secondary d-block">masters: ${salon.employees.users.length}</small>
         </td>
         <td>${salon.timezone}</td>
-        <td>${salon.employees.users.length}</td>
         <td>
-          <a class="badge badge-light" ${attrs({ href: `/${salon.alias}/booking` })}>Booking</a>
-          <a class="badge badge-light" ${attrs({ href: `/${salon.alias}/calendar` })}>Calendar</a>
+          <a class="btn btn-sm btn-light" ${attrs({ href: `/${salon.alias}/booking` })}>Booking</a>
+          <a class="btn btn-sm btn-light" ${attrs({ href: `/${salon.alias}/calendar` })}>Calendar</a>
         </td>
       </tr>`)}
     </tbody>
