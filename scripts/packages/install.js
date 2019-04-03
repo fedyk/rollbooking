d9970f385b7755bd602f59781617bf88e12437f2
Andrew <fedyk72@gmail.com>
2019-04-04 00:57:31 +0200

    Update build tasks

--- scripts/packages/install.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const { execSync } = require("child_process");
-const command = `cd ${__dirname}/../../packages/calendar && npm install`;
-
-console.log(command);
-execSync(command);
--- scripts/packages/test.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const { execSync } = require("child_process");
-const command = `cd ${__dirname}/../../packages/calendar && npm test`;
-
-console.log(command);
-execSync(command);
