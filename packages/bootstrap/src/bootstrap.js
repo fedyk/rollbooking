704063533b5903baf565fe0ff92415f9da1f9c15
Andrew <fedyk72@gmail.com>
2019-04-03 23:12:25 +0200

    Update calendar

--- packages/bootstrap/src/bootstrap.js
+++ packages/bootstrap/src/bootstrap.js
@@ -1,2 +1,10 @@
 import "./bootstrap.scss";
 // import "bootstrap";
+import jQuery from "jquery";
+import Popover from "bootstrap/js/src/popover";
+import Modal from "bootstrap/js/src/modal.js";
+// export default as Modal from "bootstrap/js/src/modal.js";
+
+export { jQuery }
+export { Popover }
+export { Modal }
