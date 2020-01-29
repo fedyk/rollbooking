599af3f1432b70f264e876bd467584c248d60e40
Andrew <fedyk72@gmail.com>
2020-01-29 07:17:58 +0100

    Show slots for service

--- src/account-profile/get-business-profile.ts
+++ src/account-profile/get-business-profile.ts
@@ -1,8 +1,9 @@
 import { Middleware } from 'koa';
+import * as tz from "timezone-support";
 import * as ejs from "ejs";
 import * as Types from '../types';
 import * as accounts from '../accounts';
-import { Booking } from '../booking';
+import { Booking, Slot } from '../booking';
 
 export const getBusinessProfile: Types.Middleware = async (ctx) => {
   const business = ctx.state.business as accounts.Business
@@ -15,10 +16,24 @@ export const getBusinessProfile: Types.Middleware = async (ctx) => {
 
   const booking = new Booking(business, [])
   const slots = booking.getNearestSlots()
+  const servicesSlots = combine(slots, business.services)
 
   ctx.body = await ejs.renderFile(`views/account-profile/business-profile.ejs`, {
     account: business,
-    slots: slots
+    services: business.services,
+    slots: slots,
+    servicesSlots: servicesSlots,
   })
 }
 
+
+function combine(slots: Slot[], services: accounts.BusinessService[]) {
+  return services.map(function (service) {
+    const serviceSlots = slots.filter(s => s.serviceId === service.id)
+    const uniqSlots = new Map<number, Slot>(serviceSlots.map(function(slot) {
+      return [tz.getUnixTime(slot.start), slot]
+    }))
+
+    return Array.from(uniqSlots.values())
+  })
+}
